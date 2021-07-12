7d58cedc52a0844df8019a19ea0b6ff2dbe4c597
Abhinay Agarwal <abhinay.agarwal@gluonhq.com>
2021-07-12 10:23:09 +0530

    Add debug information

--- .github/scripts/jpackage.sh
+++ .github/scripts/jpackage.sh
@@ -1,15 +1,19 @@
 jdeps_modules=$(jdeps --module-path $JAVAFX_HOME --print-module-deps --ignore-missing-deps app/target/lib/scenebuilder-$VERSION-all.jar)
 JAVAFX_MODULES=javafx.fxml,javafx.media,javafx.swing,javafx.web
 
+echo $jdeps_modules
+
 download_url="https://cdn.azul.com/zulu/bin/zulu16.30.19-ca-jdk16.0.1-macosx_aarch64.tar.gz"
 wget -O $RUNNER_TEMP/java_package.tar.gz $download_url
 tar -xvf $RUNNER_TEMP/java_package.tar.gz -C $RUNNER_TEMP/
 
-$JAVA_HOME/bin/jlink \
+echo "jlink start"
+$JAVA_HOME/bin/jlink -v \
 --module-path $RUNNER_TEMP/zulu16.30.19-ca-jdk16.0.1-macosx_aarch64/jmods:$JAVAFX_HOME \
 --add-modules $jdeps_modules,$JAVAFX_MODULES \
 --output app/target/runtime \
 --strip-debug --compress 2 --no-header-files --no-man-pages
+"echo jlink end"
 
 $JPACKAGE_HOME/bin/jpackage \
 --app-version $VERSION \
