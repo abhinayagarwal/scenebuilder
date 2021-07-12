3ada797bfb7557a7d0baf70e19a54a2d25c484cb
Abhinay Agarwal <abhinay.agarwal@gluonhq.com>
2021-07-12 09:39:40 +0530

    Use Aarch64 for creating custom JRE

--- .github/scripts/jpackage.sh
+++ .github/scripts/jpackage.sh
@@ -1,8 +1,12 @@
 jdeps_modules=$(jdeps --module-path $JAVAFX_HOME --print-module-deps --ignore-missing-deps app/target/lib/scenebuilder-$VERSION-all.jar)
 JAVAFX_MODULES=javafx.fxml,javafx.media,javafx.swing,javafx.web
 
+download_url="https://cdn.azul.com/zulu/bin/zulu16.30.19-ca-jdk16.0.1-macosx_aarch64.tar.gz"
+wget -O $RUNNER_TEMP/java_package.tar.gz $download_url
+tar -xvf $RUNNER_TEMP/java_package.tar.gz -C $RUNNER_TEMP/
+
 $JAVA_HOME/bin/jlink \
---module-path $JAVAFX_HOME \
+--module-path -C $RUNNER_TEMP/zulu16.30.19-ca-jdk16.0.1-macosx_aarch64/jmods/:$JAVAFX_HOME \
 --add-modules $jdeps_modules,$JAVAFX_MODULES \
 --output app/target/runtime \
 --strip-debug --compress 2 --no-header-files --no-man-pages
