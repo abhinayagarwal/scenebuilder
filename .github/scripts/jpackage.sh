5f819ec939480ad79c335ed0d061d94548176b1d
Abhinay Agarwal <abhinay.agarwal@gluonhq.com>
2021-07-12 10:11:30 +0530

    Fix jlink command

--- .github/scripts/jpackage.sh
+++ .github/scripts/jpackage.sh
@@ -6,7 +6,7 @@ wget -O $RUNNER_TEMP/java_package.tar.gz $download_url
 tar -xvf $RUNNER_TEMP/java_package.tar.gz -C $RUNNER_TEMP/
 
 $JAVA_HOME/bin/jlink \
---module-path -C $RUNNER_TEMP/zulu16.30.19-ca-jdk16.0.1-macosx_aarch64/jmods/:$JAVAFX_HOME \
+--module-path $RUNNER_TEMP/zulu16.30.19-ca-jdk16.0.1-macosx_aarch64/jmods:$JAVAFX_HOME \
 --add-modules $jdeps_modules,$JAVAFX_MODULES \
 --output app/target/runtime \
 --strip-debug --compress 2 --no-header-files --no-man-pages
